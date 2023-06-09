5f2fee257d739ada5315b8e31e020be41dd2748c
ParamSurywashi <parmanandchared1997@gmail.com>
2023-06-09 16:05:03 +0530

    new snipping wheel

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ let btn = document.getElementById("spin");
 let number = Math.ceil(Math.random() * 1000);
 
 btn.onclick = function () {
-  container.style.transform = "rotate(" + number + "deg)";
-  number += Math.ceil(Math.random() * 1000);
+    container.style.transform = "rotate(" + number + "deg)";
+    // let result = Math.ceil(number % 360 / 45);
+    // let mappedResult = (result === 0) ? 8 : result;
+  
+    // console.log("Snipping Wheel Result: " + mappedResult);
+  
+    number += Math.ceil(Math.random() * 1000);
+    console.log(number);
 }
